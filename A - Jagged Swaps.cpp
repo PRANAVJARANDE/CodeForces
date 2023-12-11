e60f69d1bef3eda4b3c45224c9fa072a35e0695e
PJARANDE <129204534+PRANAVJARANDE@users.noreply.github.com>
2023-12-11 14:11:20 +0530

    Create A - Jagged Swaps.cpp

---
+++ A - Jagged Swaps.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD (int)(1e9 + 7)
+#define ll long long
+
+void solve()
+{
+    int n;cin>>n;
+    vector<int>a(n);
+    int mini=INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+        mini=min(mini,a[i]);
+    }
+    if(a[0]==mini)
+    {
+        cout<<"YES"<<endl;
+    }    
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0) ;
+    int t;cin >> t;
+    while (t--) 
+    {
+        solve();   
+    }
+    return 0;
+}
