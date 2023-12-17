884599e077633935f14ee242ac6d0c8306db9657
PJARANDE <129204534+PRANAVJARANDE@users.noreply.github.com>
2023-12-17 17:10:23 +0530

    Create 	A - Constructive Problems.cpp

---
+++ A - Constructive Problems.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD (int)(1e9 + 7)
+#define ll long long
+ 
+void solve()
+{
+    int n,m;cin>>n>>m;
+    int x=max(n,m);
+    cout<<x<<endl;
+}
+ 
+ 
+int main() 
+{
+    ios::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0) ;
+    int t;cin >> t;
+    while (t--) 
+    {
+        solve();
+        
+    }
+    return 0;
+}
