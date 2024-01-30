922bf36143032f9e1d6e40174c7c8c96eb88afaf
PJARANDE <129204534+PRANAVJARANDE@users.noreply.github.com>
2024-01-30 23:01:27 +0530

    Create A - Brick Wall.cpp

---
+++ A - Brick Wall.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD (int)(1e9 + 7)
+#define ll long long
+
+void solve()
+{
+    ll n,m;cin>>n>>m;
+    ll ans=n*(m/2);
+    cout<<ans<<endl;
+}
+
+
+int main() 
+{
+    ios::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0) ;
+    int t;cin >> t;
+    while (t--) 
+    {
+        solve();
+    }
+    return 0;
+}
