b17578f0635e1b2818e38d9c4022b7fedbcd5ba6
PJARANDE <129204534+PRANAVJARANDE@users.noreply.github.com>
2024-05-18 14:46:45 +0530

    Create B - Cat, Fox and the Lonely Array.cpp

---
+++ B - Cat, Fox and the Lonely Array.cpp
@@ -0,0 +1,156 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pl; 
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<pair<long long,long long>> vllp;
+typedef vector<pl> vpl;
+typedef vector<vector<ll>> vvl;
+#define MOD (int)(1e9 + 7)
+#define endl "\n"
+#define sqrt sqrtl
+#define pb push_back
+#define popb pop_back
+#define pf push_front
+#define popf pop_front
+#define emb emplace_back 
+#define mp make_pair
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define all(ds) (ds).begin(), (ds).end()
+// Clear Memory
+#define clr(ds) memset(ds, 0, sizeof(ds))
+// Ascending Vector
+#define sort(ds) sort(all(ds))
+// Descending Vector
+#define rev(ds) reverse(all(ds))
+// Total Sum
+#define tsum(ds) (accumulate(all(ds), 0ll))
+// Count frequency of x
+#define ctx(ds, x) (count(all(ds), (x)))
+// Size
+#define sz(ds) ((ll)(ds).size())
+// Min Value
+#define minv(ds) (*min_element(all(ds)))
+// Max Value
+#define maxv(ds) (*max_element(all(ds)))
+// Min Value Index
+#define minIdx(ds) (min_element(all(ds)) - (ds).begin())
+// Max Value Index
+#define maxIdx(ds) (max_element(all(ds)) - (ds).begin())
+// Binary Search
+#define bs(ds, x) (binary_search(all(ds), x))
+// Lower Bound Index
+#define lob(ds, x) (lower_bound(all(ds), (x)) - (ds).begin())
+// Upper Bound Index
+#define upb(ds, x) (upper_bound(all(ds), (x)) - (ds).begin())
+// Ceil Function
+#define ceil(num, div) ((num % div != 0) ? ((num / div) + 1) : (num / div))
+
+const ll INF = 0x3f3f3f3f;              //inf
+const ll neg_INF = 0xcfcfcfcf;          //-inf
+const ld EPS = 1e-9;                    //epsilon
+const ld PI = 3.1415926535897932384626; //pi
+
+ll min(ll a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+ll pow_mod(ll a,ll b, ll m) { 
+    ll res=1;
+    while(b>0){
+        if(b&1){
+            res=(res*a)%m;
+        }
+        a=(a*a)%m;
+        b=b>>1;
+    }
+    return res;
+}
+ll modinv(ll b, ll m){ return pow_mod(b,m-2,m); }
+string to_upper(string a){ for(int i=0;i<(int)a.size();++i)if(a[i]>='a' && a[i]<='z')a[i]-='a'-'A';return a;}
+string to_lower(string a){ for(int i=0;i<(int)a.size();++i)if(a[i]>='A' && a[i]<='Z')a[i]+='a'-'A';return a;}
+
+bool checker(vll &a,ll k,ll n)
+{
+    vector<ll> f1(21,0);
+    for(int i=0;i<k;i++)
+    {
+        for(int j=0;j<=20;j++)
+        {
+            if(a[i] & (1<<j))
+            {
+                f1[j]++;
+            }
+        }
+    }
+    vector<ll> f2=f1;
+    for(int i=k;i<n;i++)
+    {
+        for(int j=0;j<=20;j++)
+        {
+            if(a[i] & (1<<j))
+            {
+                f2[j]++;
+            }
+        }
+        for(int j=0;j<=20;j++)
+        {
+            if(a[i-k] & (1<<j))
+            {
+                f2[j]--;
+            }
+        }
+        for(int i=0;i<=20;i++)
+        {
+            if(f1[i]==0 && f2[i]!=0)return 0;
+            if(f2[i]==0 && f1[i]!=0)return 0;
+        }
+    }
+    return 1;
+}
+
+void solve()
+{
+    ll n;cin>>n;
+    vll a(n);
+    for(int i=0;i<n;i++)cin>>a[i];
+
+    int l=1,r=n,ans=n;
+    while(l<=r)
+    {
+        ll mid=l+(r-l)/2;
+        if(checker(a,mid,n))
+        {
+            ans=mid;
+            r=mid-1;
+        }
+        else
+        {
+            l=mid+1;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+
+int main() 
+{
+    ios::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0) ;
+    int t;cin >> t;
+    while (t--) 
+    {
+        solve();
+    }
+    return 0;
+}
