60f2ef96b28f165805eaa1765afcd95cb953b42c
PJARANDE <pranavjarande@gmail.com>
2024-12-07 17:57:26 +0530

    Add files via upload

---
+++ G_Tree_Destruction.cpp
@@ -0,0 +1,169 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pl; 
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<pair<long long,long long>> vllp;
+typedef vector<pl> vpl;
+typedef vector<vector<ll>> vvl;
+#define MOD (int)(1e9 + 7)
+#define endl "\n"
+#define sqrt sqrtl
+#define pb push_back
+#define popb pop_back
+#define pf push_front
+#define popf pop_front
+#define emb emplace_back 
+#define mp make_pair
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+#define all(ds) (ds).begin(), (ds).end()
+// Clear Memory
+#define clr(ds) memset(ds, 0, sizeof(ds))
+// Ascending Vector
+#define sort(ds) sort(all(ds))
+// Descending Vector
+#define rev(ds) reverse(all(ds))
+// Total Sum
+#define tsum(ds) (accumulate(all(ds), 0ll))
+// Count frequency of x
+#define ctx(ds, x) (count(all(ds), (x)))
+// Size
+#define sz(ds) ((ll)(ds).size())
+// Min Value
+#define minv(ds) (*min_element(all(ds)))
+// Max Value
+#define maxv(ds) (*max_element(all(ds)))
+// Min Value Index
+#define minIdx(ds) (min_element(all(ds)) - (ds).begin())
+// Max Value Index
+#define maxIdx(ds) (max_element(all(ds)) - (ds).begin())
+// Binary Search
+#define bs(ds, x) (binary_search(all(ds), x))
+// Lower Bound Index
+#define lob(ds, x) (lower_bound(all(ds), (x)) - (ds).begin())
+// Upper Bound Index
+#define upb(ds, x) (upper_bound(all(ds), (x)) - (ds).begin())
+// Ceil Function
+#define ceil(num, div) ((num % div != 0) ? ((num / div) + 1) : (num / div))
+
+const ll INF = 0x3f3f3f3f;              //inf
+const ll neg_INF = 0xcfcfcfcf;          //-inf
+const ld EPS = 1e-9;                    //epsilon
+const ld PI = 3.1415926535897932384626; //pi
+
+ll min(ll a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+ll mod_pow(ll a,ll b, ll m) { 
+    ll res=1;
+    while(b>0){
+        if(b&1){
+            res=(res*a)%m;
+        }
+        a=(a*a)%m;
+        b=b>>1;
+    }
+    return res;
+}
+ll mod_neg(ll a,ll b)
+{
+    ll ans=((a%b)+b)%b;
+    return ans;
+}
+ll modinv(ll b, ll m){ return mod_pow(b,m-2,m); }
+string to_upper(string a){ for(int i=0;i<(int)a.size();++i)if(a[i]>='a' && a[i]<='z')a[i]-='a'-'A';return a;}
+string to_lower(string a){ for(int i=0;i<(int)a.size();++i)if(a[i]>='A' && a[i]<='Z')a[i]+='a'-'A';return a;}
+
+//Interactive problem
+ll ask(int i,int j)
+{
+    cout<<"? "<<i<<" "<<j<<endl;
+    cout.flush();
+    ll res;cin>>res;
+    return res;
+}
+
+vvl dp;
+void helper(ll in,ll par,vvl &adj,vll & deg)
+{
+    ll maxi1=-1,maxi2=-1;
+    for(auto x:adj[in])
+    {
+        if(x!=par)
+        {
+            helper(x,in,adj,deg);
+            ll ta=dp[x][0]+deg[in]-2;
+            if(dp[x][0]>maxi1)
+            {
+                maxi2=maxi1;
+                maxi1=dp[x][0];
+            }
+            else if(dp[x][0]>maxi2)
+            {
+                maxi2=dp[x][0];
+            }
+            dp[in][0]=max(dp[in][0],ta);
+            dp[in][1]=max(dp[in][1],ta);
+        }
+    }
+    if(maxi1!=-1 && maxi2!=-1)
+    {
+        dp[in][1]=maxi1+maxi2+deg[in]-4;
+    }
+}
+
+void solve()
+{
+    ll n;cin>>n;
+    vvl adj(n);
+    vll deg(n);
+    dp=vvl(n,vll(2,0));
+    for(int i=0;i<n-1;i++)
+    {
+        ll u,v;cin>>u>>v;
+        u--;
+        v--;
+        deg[u]++;
+        deg[v]++;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    for(int i=0;i<n;i++)
+    {
+        dp[i][0]=deg[i];
+        dp[i][1]=deg[i];
+    }
+    
+    helper(0,-1,adj,deg);
+    ll ans=1;
+    for(auto x:dp)
+    {
+        ans=max(ans,max(x[0],x[1]));
+    }
+    cout<<ans<<endl;
+    
+}
+
+
+int main() 
+{
+    ios::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0) ;
+    int t;cin >> t;
+    while (t--) 
+    {
+        solve();
+    }
+    return 0;
+}
