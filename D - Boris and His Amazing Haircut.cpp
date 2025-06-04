4e817bd4c39bd4f94d3cbb913972bfd683b5b336
pj17 <pranavjarande@gmail.com>
2025-06-04 15:51:46 +0530

    Create D - Boris and His Amazing Haircut.cpp

---
+++ D - Boris and His Amazing Haircut.cpp
@@ -0,0 +1,210 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pl; 
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<pair<long long,long long>> vllp;
+typedef vector<pl> vpl;
+typedef vector<vector<ll>> vvl;
+#define MOD (int)(1e9 + 7)
+#define endl "\n"
+#define sqrt sqrtl
+#define pb push_back
+#define popb pop_back
+#define pf push_front
+#define popf pop_front
+#define emb emplace_back 
+#define mp make_pair
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+
+#define yes cout << "Yes" << endl;
+#define no cout << "No" << endl;
+#define ff first
+#define ss second
+
+
+#define all(ds) (ds).begin(), (ds).end()
+// Clear Memory
+#define clr(ds) memset(ds, 0, sizeof(ds))
+// Ascending Vector
+#define sort(ds) sort(all(ds))
+// Descending Vector
+#define rev(ds) reverse(all(ds))
+// Total Sum
+#define tsum(ds) (accumulate(all(ds), 0ll))
+// Count frequency of x
+#define ctx(ds, x) (count(all(ds), (x)))
+// Size
+#define sz(ds) ((ll)(ds).size())
+// Min Value
+#define minv(ds) (*min_element(all(ds)))
+// Max Value
+#define maxv(ds) (*max_element(all(ds)))
+// Min Value Index
+#define minIdx(ds) (min_element(all(ds)) - (ds).begin())
+// Max Value Index
+#define maxIdx(ds) (max_element(all(ds)) - (ds).begin())
+// Binary Search
+#define bs(ds, x) (binary_search(all(ds), x))
+// Lower Bound Index
+#define lob(ds, x) (lower_bound(all(ds), (x)) - (ds).begin())
+// Upper Bound Index
+#define upb(ds, x) (upper_bound(all(ds), (x)) - (ds).begin())
+// Ceil Function
+#define ceil(num, div) ((num % div != 0) ? ((num / div) + 1) : (num / div))
+
+const ll INF = 0x3f3f3f3f;              //inf
+const ll neg_INF = 0xcfcfcfcf;          //-inf
+const ld EPS = 1e-9;                    //epsilon
+const ld PI = 3.1415926535897932384626; //pi
+
+ll min(ll a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+ll mod_pow(ll a,ll b, ll m) { 
+    ll res=1;
+    while(b>0){
+        if(b&1){
+            res=(res*a)%m;
+        }
+        a=(a*a)%m;
+        b=b>>1;
+    }
+    return res;
+}
+ll mod_neg(ll a,ll b)
+{
+    ll ans=((a%b)+b)%b;
+    return ans;
+}
+ll modinv(ll b, ll m){ return mod_pow(b,m-2,m); }
+string to_upper(string a){ for(int i=0;i<(int)a.size();++i)if(a[i]>='a' && a[i]<='z')a[i]-='a'-'A';return a;}
+string to_lower(string a){ for(int i=0;i<(int)a.size();++i)if(a[i]>='A' && a[i]<='Z')a[i]+='a'-'A';return a;}
+
+//Interactive problem
+ll ask(int i,int j)
+{
+    cout<<"? "<<i<<" "<<j<<endl;
+    cout.flush();
+    ll res;cin>>res;
+    return res;
+}
+
+
+ll query(int node,int i,int j,vector<ll>&tree,int l,int r)
+{
+    if(i>r || j<l)
+    {
+        return 0;
+    }
+
+    if(l<=i && j<=r)
+    {
+        return tree[node];
+    }
+    ll mid=(i+j)/2;
+    return query(2*node+1,i,mid,tree,l,r)+query(2*node+2,mid+1,j,tree,l,r);
+}
+
+void update(int node,int i,int j,vector<ll>&tree,int in)
+{
+    if(i==j)
+    {
+        tree[node]=1;
+        return;
+    }
+
+    ll mid=(i+j)/2;
+    if(in<=mid)
+    {
+        update(2*node+1,i,mid,tree,in);
+    }
+    else
+    {
+        update(2*node+2,mid+1,j,tree,in);
+    }
+    tree[node]=tree[2*node+1]+tree[2*node+2];
+}
+
+void solve()
+{
+    ll n;cin>>n;
+    vll a(n),b(n);
+    for(int i=0;i<n;i++)cin>>a[i];
+    for(int i=0;i<n;i++)cin>>b[i];
+    ll m;cin>>m;
+    map<ll,ll>freq;
+    for(int i=0;i<m;i++)
+    {
+        ll u;cin>>u;
+        freq[u]++;
+    }
+    map<ll,vll>val;
+    map<ll,vll>to_change;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]<b[i])
+        {
+            cout<<"NO"<<endl;
+            return;
+        }
+        val[b[i]].push_back(i);
+        if(a[i]!=b[i])to_change[b[i]].push_back(i);
+    }
+
+    vll v1;
+    for(auto x:val)v1.push_back(x.first);
+    sort(v1);
+    reverse(v1.begin(),v1.end());
+
+    vll tree(4*n);
+
+    for(auto num:v1)
+    {
+        if(!to_change[num].empty())
+        {
+            ll cnt=1;
+            ll pv=to_change[num][0];
+            for(int i=1;i<to_change[num].size();i++)
+            {
+                ll sum=query(0,0,n-1,tree,pv,to_change[num][i]);
+                if(sum!=0)
+                {
+                    cnt++;
+                    pv=to_change[num][i];
+                }
+            }
+            if(freq[num]<cnt)
+            {
+                cout<<"NO"<<endl;
+                return;
+            }
+        }
+        for(auto x:val[num])update(0,0,n-1,tree,x);
+    }
+    cout<<"YES"<<endl;
+}
+
+
+int main() 
+{
+    ios::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0) ;
+    int t;cin >> t;
+    while (t--) 
+    {
+        solve();
+    }
+    return 0;
+}
