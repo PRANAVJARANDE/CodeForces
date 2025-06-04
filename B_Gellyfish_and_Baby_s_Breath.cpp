913834e47db0343a7aa5b09824b328e881a3be2f
pj17 <pranavjarande@gmail.com>
2025-06-04 15:51:01 +0530

    Add files via upload

---
+++ B_Gellyfish_and_Baby_s_Breath.cpp
@@ -0,0 +1,178 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pl; 
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<pair<long long,long long>> vllp;
+typedef vector<pl> vpl;
+typedef vector<vector<ll>> vvl;
+#define MOD (int)(998244353)
+#define endl "\n"
+#define sqrt sqrtl
+#define pb push_back
+#define popb pop_back
+#define pf push_front
+#define popf pop_front
+#define emb emplace_back 
+#define mp make_pair
+#define mll map<ll, ll>
+#define sl set<ll>
+#define sc set<char>
+
+
+#define yes cout << "Yes" << endl;
+#define no cout << "No" << endl;
+#define ff first
+#define ss second
+
+
+#define all(ds) (ds).begin(), (ds).end()
+// Clear Memory
+#define clr(ds) memset(ds, 0, sizeof(ds))
+// Ascending Vector
+#define sort(ds) sort(all(ds))
+// Descending Vector
+#define rev(ds) reverse(all(ds))
+// Total Sum
+#define tsum(ds) (accumulate(all(ds), 0ll))
+// Count frequency of x
+#define ctx(ds, x) (count(all(ds), (x)))
+// Size
+#define sz(ds) ((ll)(ds).size())
+// Min Value
+#define minv(ds) (*min_element(all(ds)))
+// Max Value
+#define maxv(ds) (*max_element(all(ds)))
+// Min Value Index
+#define minIdx(ds) (min_element(all(ds)) - (ds).begin())
+// Max Value Index
+#define maxIdx(ds) (max_element(all(ds)) - (ds).begin())
+// Binary Search
+#define bs(ds, x) (binary_search(all(ds), x))
+// Lower Bound Index
+#define lob(ds, x) (lower_bound(all(ds), (x)) - (ds).begin())
+// Upper Bound Index
+#define upb(ds, x) (upper_bound(all(ds), (x)) - (ds).begin())
+// Ceil Function
+#define ceil(num, div) ((num % div != 0) ? ((num / div) + 1) : (num / div))
+
+const ll INF = 0x3f3f3f3f;              //inf
+const ll neg_INF = 0xcfcfcfcf;          //-inf
+const ld EPS = 1e-9;                    //epsilon
+const ld PI = 3.1415926535897932384626; //pi
+
+ll min(ll a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+ll mod_pow(ll a,ll b, ll m) { 
+    ll res=1;
+    while(b>0){
+        if(b&1){
+            res=(res*a)%m;
+        }
+        a=(a*a)%m;
+        b=b>>1;
+    }
+    return res;
+}
+ll mod_neg(ll a,ll b)
+{
+    ll ans=((a%b)+b)%b;
+    return ans;
+}
+ll modinv(ll b, ll m){ return mod_pow(b,m-2,m); }
+string to_upper(string a){ for(int i=0;i<(int)a.size();++i)if(a[i]>='a' && a[i]<='z')a[i]-='a'-'A';return a;}
+string to_lower(string a){ for(int i=0;i<(int)a.size();++i)if(a[i]>='A' && a[i]<='Z')a[i]+='a'-'A';return a;}
+
+//Interactive problem
+ll ask(int i,int j)
+{
+    cout<<"? "<<i<<" "<<j<<endl;
+    cout.flush();
+    ll res;cin>>res;
+    return res;
+}
+
+void solve()
+{
+    ll n;cin>>n;
+    vll p(n),q(n);
+    for(int i=0;i<n;i++)cin>>p[i];
+    for(int i=0;i<n;i++)cin>>q[i];
+
+    vll r(n);
+
+    ll maxi1=-1,in1=-1;
+    ll maxi2=-1,in2=-1;
+    for(int i=0;i<n;i++)
+    {
+        if(p[i]>maxi1)
+        {
+            maxi1=p[i];
+            in1=i;
+        }
+        if(q[i]>maxi2)
+        {
+            maxi2=q[i];
+            in2=i;
+        }
+
+        ll pin,qin;
+        if(maxi1==maxi2)
+        {
+            ll w1=i-in1;
+            ll w2=i-in2;
+
+            if(p[w2]>q[w1])
+            {
+                pin=w2;
+                qin=in2;
+            }
+            else
+            {
+                pin=in1;
+                qin=w1;
+            }
+        }
+        else if(maxi1>maxi2)
+        {
+            pin=in1;
+            qin=i-in1;
+        }
+        else
+        {
+            qin=in2;
+            pin=i-in2;
+        }
+
+        ll res=((mod_pow(2,p[pin],MOD)%MOD) + (mod_pow(2,q[qin],MOD)%MOD))%MOD;
+        r[i]=res;
+    }
+
+    for(auto x:r)
+    {
+        cout<<x<<" ";
+    }
+    cout<<endl;
+}
+
+
+int main() 
+{
+    ios::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0) ;
+    int t;cin >> t;
+    while (t--) 
+    {
+        solve();
+    }
+    return 0;
+}
